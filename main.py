55b3d26612e8abc94be67c980506c3346b48a824
Nina <44364127+ninadicara@users.noreply.github.com>
2019-02-27 16:05:52 +0000

    add working remove stop words function
    Function to remove stop words now working and returns cleaned tweets to dataframe.

--- main.py
+++ main.py
@@ -6,59 +6,89 @@ from textblob import TextBlob                # Package for sentiment analysis
 from dev_logins import *                     # User-defined file that needs to be set up before use
 from many_stop_words import get_stop_words   # Import of stop words
 
-class Collect: 
-   """Class sets up API and collects 1000 tweets"""
+
+def api_setup():
+
+   """ Uses keys and tokens from dev_logins file to setup API access. Returns Tweepy API object """
+   
+   #Use imported keys/tokens to set up API. 
+   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+   auth.set_access_token(access_token, access_token_secret)
+   api = tweepy.API(auth)
+
+   return api
+
+def collect_tweets(searchterm):
+
+   """ Collects 1000 tweets matching searchterm through the api and returns a dataframe with a column of tweets """
+
    text_lst = []
+   for tweet in tweepy.Cursor(api_setup().search, q=searchterm).items(1000):
+      text_lst.append(tweet.text)
 
-   def__init__(self):
-      api = api.self
-
-
-   def api_setup():
-      """ Uses keys and tokens from dev_logins file to setup API access. Returns Tweepy API object """
-      #Use imported keys/tokens to set up API. 
-      auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-      auth.set_access_token(access_token, access_token_secret)
-      api = tweepy.API(auth)
-
-      return api
-
-   def create_df():
-   for tweet in tweepy.Cursor(api_setup().search, q='%23engvwales+-filter%3Aretweets%27').items(1000):
-         text_lst.append(tweet.text)
-
+   #Turn list into pandas dataframe object
    tweets = pd.DataFrame(text_lst, columns=['text'])
-   tweets["Sentiment"] = ""
+   
+   return tweets
 
-class Process:
+def remove_specials(df): 
 
-   def clean_tweets(df): 
-      """ For tweets in a Pandas dataframe under a column called 'text', will remove special characters and hyperlinks"""
-      for index, row in df.iterrows():
-         msg = row['text']
-         msg = msg.lower()
-         #Regex statement to remove hyperlinks and special characters
-         msg = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", msg).split())
-         row['text'] = msg
+   """ For tweets in a Pandas dataframe under a column called 'text', will remove special characters, hyperlinks"""
 
-      return df
+   for index, row in df.iterrows():
+      msg = row['text']
+      msg = msg.lower()
+      #Regex statement to remove hyperlinks and special characters
+      msg = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", msg).split())
+      row['text'] = msg
 
-   def remove_stops(df):
-      """ For tweets in a Pandas dataframe under a column called 'text', will remove all stop words from 'many_stop_words' package """
-      stop_words = list(get_stop_words('en')) 
-      for index, row in df.iterrows():
-         msg = row['text'].split(" ")
-         newmsg = ""
-         for word in msg:
-            if word not in stop_words:
-               newmsg = newmsg + " " + word
-      return df
+   return df
 
-stop_words = list(get_stop_words('en')) 
-print(stop_words)
+def remove_stops(df):
 
-# tweets = clean_tweets(tweets)
-# tweets = remove_stops(tweets)
+   """ For tweets in a Pandas dataframe under a column called 'text' will remove all stop words from 'many_stop_words' package """
 
-# print(tweets)
+   stop_words = list(get_stop_words('en')) 
+   stop_words.append('rt') #add RT as a stopword because it does not add value to the dataset. 
+
+   for index, row in df.iterrows():
+
+      #split message into list of individual words
+      msg = row['text'].split(" ")
+      newmsg = ""
+
+      #for each word in the list, if it's not in stop_words then keep in by adding to newmsg. 
+      for word in msg:
+         if word not in stop_words:
+            newmsg = newmsg + " " + word
+
+      #reassign value of df cell as newmsg which has no stop words 
+      row['text'] = newmsg
+
+   return df
+
+def clean_tweets(df):
+
+   """ Cleans tweets in dataframe column 'tweets' ready for sentiment analysis """
+
+   df = remove_specials(df)
+   df = remove_stops(df)
+
+   return df
+
+def tweet_setup(searchterm):
+   
+   """ General function which returns a clean dataframe of 1000 tweets, matching the searchterm"""
+
+   tweets = collect_tweets(searchterm)
+   tweets = clean_tweets(tweets)
+
+   return tweets
+
+#Create a dataframe with tweets about the England Wales Rugby Game. 
+
+tweets = tweet_setup('%23engvwales+-filter%3Aretweets%27')
+
+print(tweets)
+
 
